e56d94df6db8d7ec2c0dbece30a6aa3556db044d
Leeor Aharon <leeor.aharon@gmail.com>
2018-02-13 00:15:44 +0200

    jest

--- test/game.test.js
+++ test/game.test.js
@@ -1,121 +1,111 @@
+/* eslint-env jest */
 'use strict'
-
-const tape = require('tape-catch')
-const _test = require('tape-promise').default
-const test = _test(tape)
 
 const { get, put, gets, modify } = require('../src/index')
 
-// Game rules:
-// Process a sequence of letters, keeping score according to the following:
-// 'a': +1
-// 'b': -1
-// 'c': on/off
-//
-// When the game is 'off', the score does not change. The game start 'off'.
-//
-// The game's state is represented as (Bool, Int) - on/off status and current score
+describe('State Monad - simple game example', function() {
+  // Game rules:
+  // Process a sequence of letters, keeping score according to the following:
+  // 'a': +1
+  // 'b': -1
+  // 'c': on/off
+  //
+  // When the game is 'off', the score does not change. The game start 'off'.
+  //
+  // The game's state is represented as (Bool, Int) - on/off status and current score
 
-const gameSequence = 'abcaaacbbcabbab'
-const startState = { on: false, score: 0 }
-const expectedResult = 2
+  const gameSequence = 'abcaaacbbcabbab'
+  const startState = { on: false, score: 0 }
+  const expectedResult = 2
 
-test('simple game example using the State Monad (get/put)', function(t) {
-  t.plan(1)
+  test('using get/put', function() {
+    const playGame = sequence => {
+      if (sequence.length === 0) {
+        return gets(state => state.score)
+      }
 
-  const playGame = sequence => {
-    if (sequence.length === 0) {
-      return gets(state => state.score)
+      return get().bind(state => {
+        switch (sequence.slice(0, 1)) {
+          case 'a':
+            return put(
+              state.on
+                ? Object.assign({}, state, { score: state.score + 1 })
+                : state
+            ).bind(() => playGame(sequence.slice(1)))
+          case 'b':
+            return put(
+              state.on
+                ? Object.assign({}, state, { score: state.score - 1 })
+                : state
+            ).bind(() => playGame(sequence.slice(1)))
+          case 'c':
+            return put(Object.assign({}, state, { on: !state.on })).bind(() =>
+              playGame(sequence.slice(1))
+            )
+        }
+      })
     }
 
-    return get().bind(state => {
+    const gameResult = playGame(gameSequence).evalState(startState)
+
+    expect(gameResult).toEqual(expectedResult)
+  })
+
+  test('using modify', function() {
+    const playGame = sequence => {
+      if (sequence.length === 0) {
+        return gets(state => state.score)
+      }
+
+      return modify(state => {
+        switch (sequence.slice(0, 1)) {
+          case 'a':
+            return state.on
+              ? Object.assign({}, state, { score: state.score + 1 })
+              : state
+          case 'b':
+            return state.on
+              ? Object.assign({}, state, { score: state.score - 1 })
+              : state
+          case 'c':
+            return Object.assign({}, state, { on: !state.on })
+        }
+      }).bind(() => playGame(sequence.slice(1)))
+    }
+
+    const gameResult = playGame(gameSequence).evalState(startState)
+
+    expect(gameResult).toEqual(expectedResult)
+  })
+
+  test('using recursion', function() {
+    const playGame = sequence => state => {
+      if (sequence.length === 0) {
+        return state
+      }
+
       switch (sequence.slice(0, 1)) {
         case 'a':
-          return put(
+          return playGame(sequence.slice(1))(
             state.on
               ? Object.assign({}, state, { score: state.score + 1 })
               : state
-          ).bind(() => playGame(sequence.slice(1)))
+          )
         case 'b':
-          return put(
+          return playGame(sequence.slice(1))(
             state.on
               ? Object.assign({}, state, { score: state.score - 1 })
               : state
-          ).bind(() => playGame(sequence.slice(1)))
+          )
         case 'c':
-          return put(Object.assign({}, state, { on: !state.on })).bind(() =>
-            playGame(sequence.slice(1))
+          return playGame(sequence.slice(1))(
+            Object.assign({}, state, { on: !state.on })
           )
       }
-    })
-  }
-
-  const gameResult = playGame(gameSequence).evalState(startState)
-
-  t.deepEqual(gameResult, expectedResult, 'game sequence processed correctly')
-  t.end()
-})
-
-test('simple game example using the State Monad (modify)', function(t) {
-  t.plan(1)
-
-  const playGame = sequence => {
-    if (sequence.length === 0) {
-      return gets(state => state.score)
     }
 
-    return modify(state => {
-      switch (sequence.slice(0, 1)) {
-        case 'a':
-          return state.on
-            ? Object.assign({}, state, { score: state.score + 1 })
-            : state
-        case 'b':
-          return state.on
-            ? Object.assign({}, state, { score: state.score - 1 })
-            : state
-        case 'c':
-          return Object.assign({}, state, { on: !state.on })
-      }
-    }).bind(() => playGame(sequence.slice(1)))
-  }
+    const gameResult = playGame(gameSequence)(startState).score
 
-  const gameResult = playGame(gameSequence).evalState(startState)
-
-  t.deepEqual(gameResult, expectedResult, 'game sequence processed correctly')
-  t.end()
-})
-
-test('simple game example using recursion', function(t) {
-  t.plan(1)
-
-  const playGame = sequence => state => {
-    if (sequence.length === 0) {
-      return state
-    }
-
-    switch (sequence.slice(0, 1)) {
-      case 'a':
-        return playGame(sequence.slice(1))(
-          state.on
-            ? Object.assign({}, state, { score: state.score + 1 })
-            : state
-        )
-      case 'b':
-        return playGame(sequence.slice(1))(
-          state.on
-            ? Object.assign({}, state, { score: state.score - 1 })
-            : state
-        )
-      case 'c':
-        return playGame(sequence.slice(1))(
-          Object.assign({}, state, { on: !state.on })
-        )
-    }
-  }
-
-  const gameResult = playGame(gameSequence)(startState).score
-
-  t.deepEqual(gameResult, expectedResult, 'game sequence processed correctly')
-  t.end()
+    expect(gameResult).toEqual(expectedResult)
+  })
 })
--- test/state.spec.js
+++ test/state.spec.js
@@ -1,349 +1,287 @@
+/* eslint-env jest */
 'use strict'
-
-const tape = require('tape-catch')
-const _test = require('tape-promise').default
-const test = _test(tape)
 
 const { State, Pair, get, modify, gets } = require('../src/index')
 
 const assign = o1 => o2 => Object.assign({}, o2, o1)
 
-test('runState calls wrapped function with the given state', function(t) {
-  t.plan(1)
-
+describe('State Monad', function() {
   const state = 42
 
-  const stateFn = s => {
-    t.equal(state, s)
-    return Pair(s * 2, s)
-  }
+  test('runState calls wrapped function with the given state', function() {
+    expect.assertions(1)
 
-  State(stateFn).runState(state)
-  t.end()
-})
-
-test('runState returns the result returned by the wrapped function', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const result = State(stateFn).runState(state)
-  t.deepEqual(result, stateFn(state))
-  t.end()
-})
-
-test('evalState calls wrapped function with the given state', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    t.equal(state, s)
-    return Pair(s * 2, s)
-  }
-
-  State(stateFn).evalState(state)
-  t.end()
-})
-
-test('evalState returns the value returned by the wrapped function', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const result = State(stateFn).evalState(state)
-  t.deepEqual(result, stateFn(state).fst)
-  t.end()
-})
-
-test('execState calls wrapped function with the given state', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    t.equal(state, s)
-    return Pair(s * 2, s)
-  }
-
-  State(stateFn).execState(state)
-  t.end()
-})
-
-test('execState returns the state returned by the wrapped function', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const result = State(stateFn).execState(state)
-  t.deepEqual(result, stateFn(state).snd)
-  t.end()
-})
-
-test('mapState returns a new State that maps both the result and the state of the previous computation', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  State(stateFn)
-    .mapState(mappedState => {
-      t.deepEqual(stateFn(state), mappedState)
-    })
-    .runState(state)
-  t.end()
-})
-
-test('mapState returns the result returned by the mapping function', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const mapFn = pair => {
-    return Pair(pair.fst * 2, pair.snd + 2)
-  }
-
-  const result = State(stateFn)
-    .mapState(mapFn)
-    .runState(state)
-  t.deepEqual(result, mapFn(stateFn(state)))
-  t.end()
-})
-
-test('withState returns a new State that maps the state of the next computation', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  State(stateFn)
-    .withState(mappedState => {
-      t.deepEqual(state, mappedState)
-    })
-    .runState(state)
-  t.end()
-})
-
-test('withState returns the result returned by the mapping function', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const mapFn = s => {
-    return s * 2
-  }
-
-  const result = State(stateFn)
-    .withState(mapFn)
-    .runState(state)
-  t.deepEqual(result, stateFn(mapFn(state)))
-  t.end()
-})
-
-test('map returns a new State that maps the value of the previous computation', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  State(stateFn)
-    .map(mappedValue => {
-      t.deepEqual(stateFn(state).fst, mappedValue)
-    })
-    .runState(state)
-  t.end()
-})
-
-test('map returns the value returned by the mapping function and the previous state', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const stateFn = s => {
-    return Pair(s * 2, s)
-  }
-
-  const mapFn = s => {
-    return s * 2
-  }
-
-  const result = State(stateFn)
-    .map(mapFn)
-    .runState(state)
-  t.deepEqual(result, Pair(mapFn(stateFn(state).fst), stateFn(state).snd))
-  t.end()
-})
-
-test('join flattens a recursive State', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const firstStateFn = s => {
-    return Pair(s * 2, s + 1)
-  }
-
-  const secondStateFn = a => s => {
-    return Pair(s + a, s)
-  }
-
-  const result = State(firstStateFn)
-    .map(mappedValue => {
-      return State(secondStateFn(mappedValue))
-    })
-    .join()
-    .runState(state)
-  t.deepEqual(result, Pair(84 + 43, 43))
-  t.end()
-})
-
-test('bind maps a State and joins it', function(t) {
-  t.plan(1)
-
-  const state = 42
-
-  const firstStateFn = s => {
-    return Pair(s * 2, s + 1)
-  }
-
-  const secondStateFn = a => s => {
-    return Pair(s + a, s)
-  }
-
-  const result = State(firstStateFn)
-    .bind(mappedValue => {
-      return State(secondStateFn(mappedValue))
-    })
-    .runState(state)
-  t.deepEqual(result, Pair(84 + 43, 43))
-  t.end()
-})
-
-test('app code sample (modify)', function(t) {
-  const state = {
-    parents: {
-      Robert: { lastName: 'Parr' },
-      Helen: { lastName: 'Parr' }
-    },
-    children: {
-      Violet: { lastName: 'Parr' },
-      John: { middleName: 'Jackson', lastName: 'Parr' }
+    const stateFn = s => {
+      expect(s).toEqual(state)
+      return Pair(s * 2, s)
     }
-  }
 
-  const getFamilyMember = (...args) => family =>
-    args.reduce((acc, val) => (acc && acc[val] ? acc[val] : {}), family)
-  const setFamilyMember = (path, updatedNode) => family =>
-    Object.assign(
-      {},
-      family,
-      path.reduceRight(
-        (acc, key, i) =>
-          Object.assign(
-            {},
-            {
-              [key]: Object.assign(
-                {},
-                getFamilyMember(...path.slice(0, i + 1))(family),
-                acc
-              )
-            }
-          ),
-        updatedNode
-      )
-    )
-
-  t.test('add child', function(st) {
-    const newBornName = ['children', 'Dashiel']
-    const newBorn = { lastName: 'Parr', nickname: 'Dash' }
-
-    const addFamilyMember = (name, newMember) =>
-      modify(setFamilyMember(name, newMember))
-
-    const updatedFamily = addFamilyMember(newBornName, newBorn).execState(state)
-
-    st.deepEqual(
-      updatedFamily,
-      Object.assign({}, state, {
-        children: Object.assign({}, state.children, {
-          [newBornName[1]]: newBorn
-        })
-      })
-    )
-    st.end()
+    State(stateFn).runState(state)
   })
 
-  t.test('merge nicknames', function(st) {
-    st.plan(1)
-    const childToUpdate = ['children', 'John']
-    const childDataToMerge = { nickname: 'JackJack' }
-    const parentToUpdate = ['parents', 'Helen']
-    const parentDataToMerge = { nickname: 'Elastigirl' }
+  test('runState returns the result returned by the wrapped function', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
 
-    const updateFamilyMember = (name, updateFn) =>
-      gets(getFamilyMember(...name))
-        .map(updateFn)
-        .bind(updatedMember =>
-          get().bind(() => modify(setFamilyMember(name, updatedMember)))
+    const result = State(stateFn).runState(state)
+    expect(result).toEqual(stateFn(state))
+  })
+
+  test('evalState calls wrapped function with the given state', function() {
+    const stateFn = s => {
+      expect(s).toEqual(state)
+      return Pair(s * 2, s)
+    }
+
+    State(stateFn).evalState(state)
+  })
+
+  test('evalState returns the value returned by the wrapped function', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    const result = State(stateFn).evalState(state)
+    expect(stateFn(state).fst).toEqual(result)
+  })
+
+  test('execState calls wrapped function with the given state', function() {
+    expect.assertions(1)
+
+    const stateFn = s => {
+      expect(s).toEqual(state)
+      return Pair(s * 2, s)
+    }
+
+    State(stateFn).execState(state)
+  })
+
+  test('execState returns the state returned by the wrapped function', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    const result = State(stateFn).execState(state)
+    expect(stateFn(state).snd).toEqual(result)
+  })
+
+  test('mapState returns a new State that maps both the result and the state of the previous computation', function() {
+    expect.assertions(1)
+
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    State(stateFn)
+      .mapState(mappedState => {
+        expect(stateFn(state)).toEqual(mappedState)
+      })
+      .runState(state)
+  })
+
+  test('mapState returns the result returned by the mapping function', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    const mapFn = pair => {
+      return Pair(pair.fst * 2, pair.snd + 2)
+    }
+
+    const result = State(stateFn)
+      .mapState(mapFn)
+      .runState(state)
+    expect(result).toEqual(mapFn(stateFn(state)))
+  })
+
+  test('withState returns a new State that maps the state of the next computation', function() {
+    expect.assertions(1)
+
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    State(stateFn)
+      .withState(mappedState => {
+        expect(mappedState).toEqual(state)
+      })
+      .runState(state)
+  })
+
+  test('withState returns the result returned by the mapping function', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    const mapFn = s => {
+      return s * 2
+    }
+
+    const result = State(stateFn)
+      .withState(mapFn)
+      .runState(state)
+    expect(result).toEqual(stateFn(mapFn(state)))
+  })
+
+  test('map returns a new State that maps the value of the previous computation', function() {
+    expect.assertions(1)
+
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    State(stateFn)
+      .map(mappedValue => {
+        expect(mappedValue).toEqual(stateFn(state).fst)
+      })
+      .runState(state)
+  })
+
+  test('map returns the value returned by the mapping function and the previous state', function() {
+    const stateFn = s => {
+      return Pair(s * 2, s)
+    }
+
+    const mapFn = s => {
+      return s * 2
+    }
+
+    const result = State(stateFn)
+      .map(mapFn)
+      .runState(state)
+    expect(Pair(mapFn(stateFn(state).fst), stateFn(state).snd)).toEqual(result)
+  })
+
+  test('join flattens a recursive State', function() {
+    const firstStateFn = s => {
+      return Pair(s * 2, s + 1)
+    }
+
+    const secondStateFn = a => s => {
+      return Pair(s + a, s)
+    }
+
+    const result = State(firstStateFn)
+      .map(mappedValue => {
+        return State(secondStateFn(mappedValue))
+      })
+      .join()
+      .runState(state)
+    expect(result).toEqual(Pair(84 + 43, 43))
+  })
+
+  test('bind maps a State and joins it', function() {
+    const firstStateFn = s => {
+      return Pair(s * 2, s + 1)
+    }
+
+    const secondStateFn = a => s => {
+      return Pair(s + a, s)
+    }
+
+    const result = State(firstStateFn)
+      .bind(mappedValue => {
+        return State(secondStateFn(mappedValue))
+      })
+      .runState(state)
+    expect(result).toEqual(Pair(84 + 43, 43))
+  })
+
+  describe('app code sample (modify)', function() {
+    const state = {
+      parents: {
+        Robert: { lastName: 'Parr' },
+        Helen: { lastName: 'Parr' }
+      },
+      children: {
+        Violet: { lastName: 'Parr' },
+        John: { middleName: 'Jackson', lastName: 'Parr' }
+      }
+    }
+
+    const getFamilyMember = (...args) => family =>
+      args.reduce((acc, val) => (acc && acc[val] ? acc[val] : {}), family)
+    const setFamilyMember = (path, updatedNode) => family =>
+      Object.assign(
+        {},
+        family,
+        path.reduceRight(
+          (acc, key, i) =>
+            Object.assign(
+              {},
+              {
+                [key]: Object.assign(
+                  {},
+                  getFamilyMember(...path.slice(0, i + 1))(family),
+                  acc
+                )
+              }
+            ),
+          updatedNode
         )
+      )
 
-    const updatedFamily = updateFamilyMember(
-      childToUpdate,
-      assign(childDataToMerge)
-    )
-      .bind(() => updateFamilyMember(parentToUpdate, assign(parentDataToMerge)))
-      .execState(state)
+    test('add child', function() {
+      const newBornName = ['children', 'Dashiel']
+      const newBorn = { lastName: 'Parr', nickname: 'Dash' }
 
-    st.deepEqual(
-      updatedFamily,
-      Object.assign({}, state, {
-        parents: Object.assign({}, state.parents, {
-          [parentToUpdate[1]]: Object.assign(
-            {},
-            state.parents[parentToUpdate[1]],
-            parentDataToMerge
-          )
-        }),
-        children: Object.assign({}, state.children, {
-          [childToUpdate[1]]: Object.assign(
-            {},
-            state.children[childToUpdate[1]],
-            childDataToMerge
-          )
+      const addFamilyMember = (name, newMember) =>
+        modify(setFamilyMember(name, newMember))
+
+      const updatedFamily = addFamilyMember(newBornName, newBorn).execState(
+        state
+      )
+
+      expect(updatedFamily).toEqual(
+        Object.assign({}, state, {
+          children: Object.assign({}, state.children, {
+            [newBornName[1]]: newBorn
+          })
         })
-      })
-    )
-    st.end()
-  })
+      )
+    })
 
-  t.end()
+    test('merge nicknames', function() {
+      const childToUpdate = ['children', 'John']
+      const childDataToMerge = { nickname: 'JackJack' }
+      const parentToUpdate = ['parents', 'Helen']
+      const parentDataToMerge = { nickname: 'Elastigirl' }
+
+      const updateFamilyMember = (name, updateFn) =>
+        gets(getFamilyMember(...name))
+          .map(updateFn)
+          .bind(updatedMember =>
+            get().bind(() => modify(setFamilyMember(name, updatedMember)))
+          )
+
+      const updatedFamily = updateFamilyMember(
+        childToUpdate,
+        assign(childDataToMerge)
+      )
+        .bind(() =>
+          updateFamilyMember(parentToUpdate, assign(parentDataToMerge))
+        )
+        .execState(state)
+
+      expect(updatedFamily).toEqual(
+        Object.assign({}, state, {
+          parents: Object.assign({}, state.parents, {
+            [parentToUpdate[1]]: Object.assign(
+              {},
+              state.parents[parentToUpdate[1]],
+              parentDataToMerge
+            )
+          }),
+          children: Object.assign({}, state.children, {
+            [childToUpdate[1]]: Object.assign(
+              {},
+              state.children[childToUpdate[1]],
+              childDataToMerge
+            )
+          })
+        })
+      )
+    })
+  })
 })
